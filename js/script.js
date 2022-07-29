f6d4c5f41cc25fdc1e2f2579137431eda86c60e7
Catalinaamm <catalinamendozamedina@gmail.com>
2022-07-29 13:38:29 +0200

    v1 menu navegacion

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const burger = document.querySelector(".burger")
+
+function visualizarLista(){
+    const lista = document.querySelector(".lista");
+    const lista__li = document.querySelectorAll(".lista__li");
+    console.log(lista__li)
+    lista.classList.toggle("lista--activada");
+    burger.classList.toggle("toggle");
+    lista__li.forEach((li,index) => (li.style.transition = `all ${1*index + 0.9}s`));
+}
+
+burger.addEventListener("click",visualizarLista)
