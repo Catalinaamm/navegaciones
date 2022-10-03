1028e526a09c2296e304c3dc2e056226a6d02fcb
Catalinaamm <catalinamendozamedina@gmail.com>
2022-10-03 12:57:13 +0200

    final

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const burger = document.querySelector(".burger")
+const burger = document.querySelector(".imagx")
 
 function visualizarLista(){
     const lista = document.querySelector(".lista");
@@ -10,3 +10,39 @@ function visualizarLista(){
 }
 
 burger.addEventListener("click",visualizarLista)
+
+let imagen = document.querySelector(".imagx");
+let negro = document.querySelector(".color5");
+let color = document.querySelector(".titulo")
+let igual = document.querySelector(".enorden")
+let nav = document.querySelector(".nav")
+let foo = document.querySelector(".foo")
+
+negro.addEventListener("click", colorNegro);
+imagen.addEventListener("click", cambiarColor);
+color.addEventListener( "click", colorSalmon);
+igual.addEventListener("click", colorIgual);
+nav.addEventListener("click",cambiarNav);
+foo.addEventListener("click", footer);
+
+function footer(){
+    foo.style.backgroundColor = "#723f39"
+}
+
+function cambiarNav(){
+    nav.style.backgroundColor = "#723f39"
+}
+
+function colorIgual(){
+    igual.style.backgroundColor = "#723f39"
+}
+
+function colorSalmon(){
+    color.style.backgroundColor = "#723f39"
+}
+function colorNegro(){
+    negro.style.backgroundColor = "#e9a18b"
+}
+function cambiarColor(){
+    imagen.style.backgroundColor = "#e9a18b" ;
+}
